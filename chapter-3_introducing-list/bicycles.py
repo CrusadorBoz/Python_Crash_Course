4b6338b45509e8e8fad8e1cfc205209e3efbf6f9
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-01-30 21:37:16 -0600

    update for python crash

--- chapter-3_introducing-list/bicycles.py
+++ chapter-3_introducing-list/bicycles.py
@@ -1,3 +1,13 @@
 # [] indicate a list.  Individual elements are separated by commas
 bicyles = ['trek', 'cannaondale', 'redline', 'specialized']
 print(bicyles)
+
+# To access elements in a list
+print(bicyles[1])
+print(bicyles[1].title())  # You can print in a more formal way.  Cap first letter
+print(bicyles[-1])  # Use the -1 to return the last element in the list
+
+# You can use individual items from a list in a sentence
+message = f"My first bicycle was a {bicyles[1].title()}"
+
+print(message)
---
+++ chapter-3_introducing-list/motorcycles.py
@@ -0,0 +1,9 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+# Adding items to the list
+motorcycles.append('ducati')
+print(motorcycles)
