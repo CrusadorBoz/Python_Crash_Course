894d7be5683cc864ab001cbbaf2faa236d3da641
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-02-01 06:16:25 -0600

    update files

--- chapter-3_introducing-list/motorcycles.py
+++ chapter-3_introducing-list/motorcycles.py
@@ -9,3 +9,14 @@ print(motorcycles)
 # Adding items to the list
 motorcycles.append('ducati')
 print(motorcycles)
+
+# You can create a blank list and add later
+motorcycle_companies = []
+motorcycle_companies.append('honda')
+motorcycle_companies.append('kawasaki')
+motorcycle_companies.append('susuki')
+print(motorcycle_companies)
+
+# You can insert at any postion
+motorcycle_companies.insert(2, 'yamaha')
+print(motorcycle_companies)
