3e08352378dc6805980332ffdbc297587cdb2889
Gordon <dowspirit@gmail.com>
2024-02-02 05:50:35 -0600

    Update Files

--- chapter-3_introducing-list/bicycles.py
+++ chapter-3_introducing-list/bicycles.py
@@ -4,7 +4,7 @@ print(bicyles)
 
 # To access elements in a list
 print(bicyles[1])
-print(bicyles[1].title())  # You can print in a more formal way.  Cap first letter
+print(bicyles[3].title())  # You can print in a more formal way.  Cap first letter
 print(bicyles[-1])  # Use the -1 to return the last element in the list
 
 # You can use individual items from a list in a sentence
