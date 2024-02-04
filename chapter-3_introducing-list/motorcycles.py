523d83fedd8d692b7a46d1bdf2e641335121b480
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-02-03 18:14:06 -0600

    update files

--- chapter-3_introducing-list/motorcycles.py
+++ chapter-3_introducing-list/motorcycles.py
@@ -20,3 +20,17 @@ print(motorcycle_companies)
 # You can insert at any postion
 motorcycle_companies.insert(2, 'yamaha')
 print(motorcycle_companies)
+
+# You can delete a element at a position
+del motorcycle_companies[2]
+
+print(motorcycle_companies)
+
+# You can use pop() to remove the last element in the list.  
+popped_MoCompanies = motorcycle_companies.pop()
+print(motorcycle_companies)
+print(popped_MoCompanies)
+
+# We can use the pop to print a sequece like if the motorcycles where in the order you owned them
+first_owned = motorcycles.pop(0)
+print(f"This first brand of motorcyle I owned was a {first_owned.title()}.")
