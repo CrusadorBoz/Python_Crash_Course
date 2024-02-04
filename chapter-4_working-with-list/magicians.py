5228f09de86ccd8908aeb89fe308fb527f292cda
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-02-04 13:06:12 -0600

    update files

--- chapter-4_working-with-list/magicians.py
+++ chapter-4_working-with-list/magicians.py
@@ -1,3 +1,8 @@
 magicians = ['alice', 'david', 'carolina']
+# A good way to read a for loop, "For every magician in the list of magicians, 
+# print the magicians name"
 for magician in magicians:
-    print(magician)
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I cannot wait to see your next trick, {magician.title()}.\n")
+
+print("Thank you, everyone.  That was a great show!")
