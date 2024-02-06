eab4bbfaa4acf8677dd52beb502b0d51c1a9275f
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-02-06 08:03:50 -0600

    update files

---
+++ chapter-4_working-with-list/first_numbers.py
@@ -0,0 +1,9 @@
+# To show a series of numbers use the range() function
+# Below only prints 1 through 4.  Another off-by-one behavior.
+for value in range(1, 5):
+    print(value)
+
+print("------")
+# To print actual 1 through 5
+for value in range(1, 6):
+    print(value)
