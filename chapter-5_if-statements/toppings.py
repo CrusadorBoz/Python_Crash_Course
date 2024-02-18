c56133d382fd42927e81bd487502cddc1bf8e74e
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-02-18 17:38:46 -0600

    update files

--- chapter-5_if-statements/toppings.py
+++ chapter-5_if-statements/toppings.py
@@ -1,4 +1,32 @@
-requested_topping = 'mushroom'
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
 
-if requested_topping != 'anchovies':
-    print("Hold the anchovies")
+# We cannot use elif on this one because it would break out and not work.
+if 'mushrooms' in requested_toppings:
+    print("Adding Mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print('Adding Pepperoni.')
+if 'extra cheese' in requested_toppings:
+    print('Adding Extra Cheese.')
+
+print(f"\nWe have finished making your pizza\n")
+
+
+# printing all the requested items. Checking for special items
+for requested_toppings in requested_toppings:
+    if requested_toppings == 'green peppers':
+        print("Sorry we are out of green peppers right now")
+    else:
+        print(f"Adding {requested_toppings}")
+
+print(f"\nWe have finished making your pizza\n\n")
+
+
+# Checking if list is empty
+toppings_requested = []
+
+if toppings_requested:
+    for toppings_request in toppings_requested:
+        print(f"Adding {toppings_requested}.")
+    print(f"\nWe have finished making your pizza")
+else:
+    print("Are you sure you want a plain pizza")
