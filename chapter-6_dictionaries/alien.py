7a2044d8947ee1d6094d363747dad7e1e1cbd828
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-02-20 18:25:57 -0600

    update files

---
+++ chapter-6_dictionaries/alien.py
@@ -0,0 +1,15 @@
+alien_0 = {'color': 'green', 'points': 5}
+
+# print(alien_0['color'])
+#print(alien_0['points'])
+print(alien_0)
+
+new_points = alien_0['points']
+# print(f"You just earned {new_points} points!")
+
+# Adding new key-value pairs
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+# You can start with an empty dictionary and add to it later.
