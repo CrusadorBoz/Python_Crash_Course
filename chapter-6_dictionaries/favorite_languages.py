7dcf49d78b93eac3a1abb3f7e5102e6401caefa5
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-03-03 06:20:56 -0600

    Update Files

--- chapter-6_dictionaries/favorite_languages.py
+++ chapter-6_dictionaries/favorite_languages.py
@@ -30,3 +30,39 @@ print("----------------------------------------------------")
 
 for value in favorite_languages.values():
     print(value.title())
+
+print("----------------------------------------------------")
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi, {name.title()}")
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}")
+
+
+if 'erin' not in favorite_languages.keys():
+    print(f"Erin, please take our pool")
+
+print("----------------------------------------------------")
+
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you for taking the poll.")
+
+print("----------------------------------------------------")
+
+print(f"The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+print("----------------------------------------------------")
+
+print(f"The following languages have been mentioned:")
+for language in set(favorite_languages.values()):   # The set gets a colleciton that is unique.  No duplicates
+    print(language.title())
+
+print("----------------------------------------------------")
+
+languages = {'python', 'rust', 'python', 'c'}  # This is a set.  Does not allow duplicates.  Looks like a Dictionary
+print(languages)
