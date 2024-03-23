5a9deef95994f3b8ed575961ae17e8839c10a017
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-03-23 09:06:39 -0500

    Update Files

---
+++ chapter-7_user input and while loops/rollercoaster.py
@@ -0,0 +1,15 @@
+height = input("How tall are you, in inches? ")
+height = int(height)
+
+if height >= 48:
+    print("\nYou are tall enough to ride!")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
+
+print("-------------------------------------------")
+
+# The module operator:
+# This divides one number by another number and gives the remainder only.
+
+tac = 4 % 3
+print(tac)
