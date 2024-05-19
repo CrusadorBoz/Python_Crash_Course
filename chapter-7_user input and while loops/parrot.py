a30f159acf3bfdb5dd766d54ac3424fa487376ae
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-05-19 05:12:41 -0500

    Update Files

--- chapter-7_user input and while loops/parrot.py
+++ chapter-7_user input and while loops/parrot.py
@@ -4,13 +4,30 @@
 
 # ------------------------------------------------------------
 
+# prompt = "Tell me something, and I will repeat it back to you."
+# prompt += "\nEnter 'quit' to end the program. "
+# prompt += "\n: "
+
+# message = ""
+# while message != 'quit':
+#     message = input(prompt)
+    
+#     if message != 'quit':
+#         print(f"You said, '{message}'.\n")
+
+# ------------------------------------------------------------
+
 prompt = "Tell me something, and I will repeat it back to you."
 prompt += "\nEnter 'quit' to end the program. "
 prompt += "\n: "
 
 message = ""
-while message != 'quit':
+active = True
+
+while active: 
     message = input(prompt)
     
-    if message != 'quit':
+    if message == 'quit':
+        active = False
+    else:
         print(f"You said, '{message}'.\n")
