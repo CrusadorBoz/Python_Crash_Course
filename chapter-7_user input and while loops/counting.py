15370ea85d43c9b3f9f596cb3b0de80bbdcba677
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2024-08-04 06:23:15 -0500

    Update Files

--- chapter-7_user input and while loops/counting.py
+++ chapter-7_user input and while loops/counting.py
@@ -5,10 +5,25 @@
 
 # ------------------------------------------------------------
 
-current_number = 0
-while current_number < 10:
-    current_number += 1
-    print(current_number)
-    if current_number % 2 == 0:
-        # print(current_number)
-        continue
+# current_number = 0
+# while current_number < 10:
+#     current_number += 1
+#     # print(current_number)
+#     if current_number % 2 == 0:
+#         # print(current_number)
+#         continue
+#     print(current_number)
+
+# ------------------------------------------------------------
+# Simple loop to count to five. 
+x = 1
+while x <= 5:
+    print(x)
+    x += 1
+
+### This loop will run forever. ###
+    # x = 1
+    # while x <= 5:
+    #     print(x)
+
+# ------------------------------------------------------------
